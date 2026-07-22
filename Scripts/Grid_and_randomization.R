780cdacbc0f466b595f94585b022488df3431cf0
SarahBassing <sarah.bassing@gmail.com>
2026-07-22 10:14:05 -0600

    updated 2026 tracks, streamline formatting, add fenceline data, update size classifications and GRTS sampling scheme to oversample

--- Scripts/Grid_and_randomization.R
+++ Scripts/Grid_and_randomization.R
@@ -17,6 +17,7 @@
   #'  Load libraries
   library(sf)
   library(terra)
+  library(nngeo)
   library(units)
   library(spsurvey)
   library(mapview)
@@ -32,12 +33,17 @@
   
   #'  Review data layers in .gpx file
   st_layers("./Spatial/BRR_pdogs/2026/Tracks_num.gpx")  
+  st_layers("./Spatial/BRR_pdogs/2026/Colonies.gpx")
   #'  Load tracks layer for mapping 2026 colonies
   pd_2026 <- st_read("./Spatial/BRR_pdogs/2026/Tracks_num.gpx", layer = "tracks") %>%
     dplyr::select(c(name, geometry))
+  pd_2026_final <- st_read("./Spatial/BRR_pdogs/2026/Colonies.gpx", layer = "tracks") %>%
+    dplyr::select(c(name, geometry))
+  pd_2026_col19 <- st_read("./Spatial/BRR_pdogs/2026/Colony 19.gpx", layer = "tracks") %>%
+    dplyr::select(c(name, geometry))
   
   #'  Grab geographic coordinate system of pd_2026 (WGS84)
-  wgs84 <- crs(pd_2026)
+  wgs84 <- crs(pd_2026_final)
   
   #'  --------------------------
   ####  Format colony polygons  ####
@@ -75,18 +81,50 @@
            colony_id = ifelse(name == "7", "7-inactive", colony_id)) %>%
     #'  Re-project to be consistent with historic data - EPSG:32614 is WGS 84 / UTM Zone 14N
     st_transform(., crs = "EPSG:32615") 
+  pd_2026_final <- pd_2026_final %>%
+    #'  Append colony 19 tracks to larger 2026 dataset
+    bind_rows(pd_2026_col19) %>%
+    #'  Assign same colony ID to colonies with multiple polygons
+    mutate(colony = name,
+           colony = ifelse(grepl("2-", name), "Colony 2", colony),
+           colony = ifelse(grepl("8-", name), "Colony 8", colony),
+           colony = ifelse(grepl("10-", name), "Colony 10", colony),
+           colony = ifelse(grepl("11-", name), "Colony 11", colony),
+           colony = ifelse(grepl("12-", name), "Colony 12", colony),
+           colony = ifelse(grepl("17-", name), "Colony 17", colony),
+           colony = ifelse(grepl("21-", name), "Colony 21", colony),
+           colony = ifelse(grepl("22-", name), "Colony 22", colony),
+           colony = ifelse(grepl("23-", name), "Colony 23", colony),
+           colony = ifelse(grepl("24-", name), "Colony 24", colony),
+           colony = ifelse(grepl("25-", name), "Colony 25", colony),
+           colony = ifelse(grepl("26-", name), "Colony 26", colony), # NEED TO DOUBLE CHECK THIS ONE
+           colony = ifelse(grepl("27-", name), "Colony 27", colony),
+           colony = ifelse(grepl("29-", name), "Colony 29", colony),
+           colony = ifelse(grepl("35-", name), "Colony 35", colony),
+           # colony = ifelse(name == "Coony 26-3", "Colony 26???", colony),
+           colony_id = substring(colony, 8),
+           colony_id = as.numeric(colony_id)) %>%
+    #'  Re-project to be consistent with historic data - EPSG:32614 is WGS 84 / UTM Zone 14N
+    st_transform(., crs = "EPSG:32615") 
   #'  Fix topology issues that arise from creating geometries with GPS tracking data
   pd_2026_fix <- pd_2026 %>%
     st_make_valid(.) 
+  pd_2026_final_fix <- pd_2026_final %>%
+    st_make_valid(.) 
   #'  Review what types of geometries are included
-  table(st_geometry_type(pd_2026_fix))    # should be all MULTILINESTRING
+  table(st_geometry_type(pd_2026_fix))          # should be all MULTILINESTRING
+  table(st_geometry_type(pd_2026_final_fix))    # should be all MULTILINESTRING
   #'  Remove any tiny or degenerated bits 
   pd_2026_fix <- pd_2026_fix[!st_is_empty(pd_2026_fix), ]
+  pd_2026_final_fix <- pd_2026_final_fix[!st_is_empty(pd_2026_final_fix), ]
   #'  Convert to polygons
   pd_2026_fix <- pd_2026_fix %>%
     st_cast(., "POLYGON") 
+  pd_2026_final_fix <- pd_2026_final_fix %>%
+    st_cast(., "POLYGON")
   #'  Visualize colony polygons
   mapview(pd_2026_fix, zcol = "colony_id")
+  mapview(pd_2026_final_fix, zcol = "colony_id")
   
   #'  Join slightly non-overlapping polygons from same colony to create single 
   #'  polygon per colony (typically arises when a fence line cuts through a colony)
@@ -107,25 +145,42 @@
     mutate(acres = as.numeric(set_units(st_area(.), "acres")),
            acres = round(acres, 4)) %>%
     relocate(acres, .before = geometry)
+  pd_2026_final_union <- pd_2026_final_fix %>%
+    #'  Make sure spatial data are valid
+    st_make_valid() %>%
+    #'  Buffer polygons by gap_distance
+    st_buffer(dist = gap_distance) %>%
+    #'  Group by colony ID so only polygons from a single colony get merged
+    group_by(colony_id) %>%
+    #'  Union the geometries for grouped polygons
+    summarise(geometry = st_union(geometry)) %>%
+    #'  Remove the buffered distance
+    st_buffer(dist = -gap_distance) %>%
+    #'  Calculate area in acres 
+    mutate(acres = as.numeric(set_units(st_area(.), "acres")),
+           acres = round(acres, 4)) %>%
+    relocate(acres, .before = geometry)
   
   #'  Review colony polygons before and after union
   mapview(pd_2026_fix)
   mapview(pd_2026_union, zcol = "colony_id")
+  mapview(pd_2026_final_fix)
+  mapview(pd_2026_final_union, zcol = "colony_id")
   
   #'  Visualize historic and mapped 2026 colonies
   mapview::mapview(pd_2023, zcol = "colony_name")  
   mapview::mapview(pd_2024, zcol = "colony_name")  
-  mapview::mapview(pd_2026_union, zcol = "colony_id")    
+  mapview::mapview(pd_2026_final_union, zcol = "colony_id")    
   #'  Compare current colonies with previously mapped colonies by overlaying them
-  mapview::mapview(pd_2024) + mapview::mapview(pd_2026_union, zcol = "colony_id")
+  mapview::mapview(pd_2024) + mapview::mapview(pd_2026_final_union, zcol = "colony_id")
   
   #'  Save cleaned up 2026 colony polygons as csv
-  pd_2026_clean <- as.data.frame(pd_2026_union) %>%
+  pd_2026_clean <- as.data.frame(pd_2026_final_union) %>%
     dplyr::select(-geometry)
   write_csv(pd_2026_clean, "./Spatial/BRR_pdogs/2026/pd_2026_colony_attributes.csv")
   
-  #'  Review spatial extent of colonies (currently using 2024 data b/c more colonies mapped than 2026)
-  st_bbox(pd_2024) # WGS 84 / UTM 14N
+  #'  Review spatial extent of colonies (originally used 2024 data b/c more colonies were mapped than early 2026)
+  st_bbox(pd_2026_final) # WGS 84 / UTM 14N
   
   #'  -------------------------------------------
   ####  Superimpose grid across active colonies  ####
@@ -273,7 +328,8 @@
     return(sample_pts)
   }
   sample_pts_2024 <- convert_to_gpx(random_cells_2024_sf)
-  sample_pts_2026 <- convert_to_gpx(random_cells_2026_1and5_sf)
+  sample_pts_2026 <- convert_to_gpx(random_cells_2026_sf)
+  # sample_pts_2026 <- convert_to_gpx(random_cells_2026_1and5_sf)
   
   #'  Export as .GPX file format
   write_sf(sample_pts_2024, dsn = paste0("./Spatial/Exported_data/random_points_2024_", Sys.Date(), ".gpx"), dataset_options = "GPX_USE_EXTENSIONS=YES")
@@ -282,55 +338,76 @@
   #'  Convert colony polygons to .KML file for OnX and Garmin GPS units
   colony_polygons_2024 <- pd_2024 %>%
     st_transform(., crs = "EPSG:4326")
-  colony_polygons_2026 <- pd_2026_union %>%
+  colony_polygons_2026 <- pd_2026_final_union %>%
     st_transform(., crs = "EPSG:4326") %>%
     dplyr::select(-acres)
-  colony_polygons_2026_1and5 <- pd_2026_union %>%
-    filter(colony_id == 1 | colony_id == 5) %>%
+  colony_polygons_2026_12 <- pd_2026_final_union %>%
+    filter(colony_id == 12) %>%
+    st_remove_holes(.) %>%
     st_transform(., crs = "EPSG:4326") %>%
-    dplyr::select(-acres)
+    dplyr::select(-acres) %>%
+    st_make_valid(.)
   write_sf(colony_polygons_2024, "./Spatial/Exported_data/colony_polygons_2024.kml", dataset_options = "GPX_USE_EXTENSIONS=YES")
   write_sf(colony_polygons_2026, "./Spatial/Exported_data/colony_polygons_2026.kml", dataset_options = "GPX_USE_EXTENSIONS=YES")
-  write_sf(colony_polygons_2026_1and5, "./Spatial/Exported_data/colony_polygons_2026_1and5.kml", dataset_options = "GPX_USE_EXTENSIONS=YES")
-  
+  write_sf(colony_polygons_2026_12, "./Spatial/Exported_data/colony_polygons_2026_12.kml", dataset_options = "GPX_USE_EXTENSIONS=YES")
+ 
   #'  Save polygons as shapefiles
-  st_write(pd_2026_union, dsn = paste0("./Spatial/BRR_pdogs/2026/BTPD_colonies_2026_", Sys.Date(), ".shp"))
+  st_write(pd_2026_final_union, dsn = paste0("./Spatial/BRR_pdogs/2026/BTPD_colonies_2026_", Sys.Date(), ".shp"))
   
   #'  -------------------------------
   ####  Treatment and Control sites  ####
   #'  -------------------------------
-  #'  Append colony attributes to spatial data
-  set.seed(963)
-  #'  "Legacy" colonies to have raptor perches (per ranch request): 10, 15, 26, 29?
-  colony_dat <- pd_2026_union %>%
-    mutate(legacy_colony = ifelse(colony_id == 10 | colony_id == 15 | 
-                                    colony_id == 26, "legacy_site", NA)) %>% 
-    #'  Remove portion of colony 7 that is inactive (as of July 2026)
-    filter(colony_id != "7-inactive") %>%
+  #'  Load fence line data
+  fences <- read_csv("./Data/BRR_colony_fencelines_2026.csv") %>%
+    rename("colony_id" = "Colony Number") %>%
+    rename("fence_present" = "Fencline Presence") %>%
+    rename("colony_description" = "Colony Discription") %>%
+    dplyr::select(-`Colony Size`) %>%
+    mutate(fence_present = ifelse(fence_present == "?", NA, fence_present)) %>%
+    mutate(fence_present = ifelse(colony_id == 40, "N", fence_present))
+  
+  #'  "Legacy" colonies to have raptor perches (per ranch request): 2, especially 4, and most importantly 6, 13 
+  colony_dat <- pd_2026_final_union %>%
+    mutate(legacy_colony = ifelse(colony_id == 2 | colony_id == 4 | 
+                                    colony_id == 6 | colony_id == 13, "legacy_site", NA)) %>% 
+    #' #'  Remove portion of colony 7 that is inactive (as of July 2026)
+    #' filter(colony_id != "7-inactive") %>%
     #'  Ensure spatial data for polygons are valid for next step
     st_make_valid() %>%
-    #'  Randomly assign whether each colony has a fence FOR NOW! REMOVE ONCE REAL FENCE DATA IS INCLUDED
-    group_by(colony_id) %>%
-    mutate(fence_present = sample(c("Yes", "No"), size = 1, prob = c(0.6, 0.4))) %>% #### REMOVE THIS ONCE REAL FENCE LINE DATA ARE ADDED
-    ungroup() 
+    #'  Append colony attributes to spatial data
+    full_join(fences, by = "colony_id") %>%
+    #'  Remove rows with fence data but no spatial data 
+    filter(!is.na(acres)) %>%
+    #'  Remove rows with spatial data but no fence data
+    filter(!is.na(fence_present))
     
   #'  Classify colony size into small, medium, and large based on acreage
   summary(colony_dat$acres)
-  hist(colony_dat$acres, breaks = 20)
+  hist(colony_dat$acres, breaks = 50)
   colony_dat <- colony_dat %>%
-    mutate(colony_size = ifelse(acres < 15, "Small", "Large"),
-           colony_size = ifelse(acres >= 15 & acres < 60, "Medium", colony_size)) %>%
-    relocate(geometry, .after = colony_size) %>%
-    arrange(colony_size)
+    #'  Small = 0 - 20 acres, Medium = 20 - 60 acres, Large = 60 - 100 acres
+    #'  In reality, Medium = ~20-40 acres and Large = ~70-95 acres
+    #'  All size classes capture about a 20-acre range in colony sizes
+    mutate(colony_size = ifelse(acres < 20, "Small", "Large"),
+           colony_size = ifelse(acres >= 20 & acres < 60, "Medium", colony_size)) %>%
+    arrange(colony_size, acres) %>%
+    relocate(colony_size, .after = acres) %>%
+    relocate(geometry, .after = colony_description) 
   table(colony_dat$colony_size)
   
-  #' #'  Make up an extra large colony with no fence line so there's at least one 
-  #' #'  colony for each block combo in the data set for now
-  #' extra_colony <- colony_dat[colony_dat$acres > 90,]
-  #' extra_colony$colony_id <- "100"
-  #' extra_colony$fence_present <- "Yes"
-  #' colony_dat <- bind_rows(colony_dat, extra_colony)
-  #' ####  GET RID OF THIS ONCE A 4TH LARGE SITE IS MAPPED  ####
+  #'  Turns our all medium-sized colonies have fences - need to fake a few without for
+  #'  GRTS randomization approach to work below so faking some colonies lacking fences
+  #'  Set seed and randomly draw which colonies do and do not have fences
+  set.seed(963)
+  new_fence <- colony_dat %>%
+    dplyr::select(c(colony_id, colony_size)) %>%
+    group_by(colony_id) %>%
+    mutate(fence_present = sample(c("Y", "N"), size = 1, prob = c(0.5, 0.5))) %>%
+    ungroup
+  #'  Append fake fence data to colony data for only medium-sized colonies
+  colony_dat <- colony_dat %>%
+    mutate(fence_present_fake = ifelse(colony_size == "Medium", new_fence$fence_present[new_fence$colony_size == "Medium"], fence_present)) %>%
+    relocate(fence_present_fake, .after = fence_present) 
   
   #'  Grab centroid of each polygon (needed for GRTS step below)
   colony_centroids_sf <- st_centroid(colony_dat)
@@ -343,31 +420,34 @@
   #'  --> 6 treatment and 6 control sites
   #'  -----------------------------
   #'  Set seed for reproducibility
-  set.seed(45398)
+  set.seed(97146) #9746 #45975
   #'  Generalized random tessellation stratified (GRTS) sample with legacy sites
   sample_set <- spsurvey::grts(
     sframe = colony_centroids_sf,                  # finite set of spatial points to choose from
     n_base = c(Small = 4, Medium = 4, Large = 4),  # sample size for each strata
     stratum_var = "colony_size",                   # column name of strata
-    caty_var = "fence_present",                    # column name of second strata with unequal sampling probability (even though in this case it is equal)
-    caty_n = c(Yes = 2, No = 2),                   # vector of sample sizes per second strata (equal sampling probability in this case)
+    caty_var = "fence_present_fake",               # column name of second strata with unequal sampling probability (even though in this case it is equal)
+    caty_n = c(Y = 2, N = 2),                      # vector of sample sizes per second strata (equal sampling probability in this case)
     legacy_var = "legacy_colony",                  # column name of legacy indicator (non-legacy colonies are NA)
-    # n_over = 4, # Must have sufficient colonies in each block for this to work -- Large colonies are an issue here
-    DesignID = "Colony_id"                         # naming structure for each site's identifier selected in the sample
+    n_over = 2,                                    # over sample in each size strata - Must have sufficient colonies in each n_base strata for this to work 
+    DesignID = "colony_id"                         # naming structure for each site's identifier selected in the sample
   )
+  #'  Review named lists within sample_set
+  names(sample_set)
   
   #'  Selected colonies
   legacy_centroids <- sample_set$sites_legacy
   random_centroids <- sample_set$sites_base
-  experiment_centroids <- bind_rows(legacy_centroids, random_centroids) %>%
-    dplyr::select(c(colony_id, acres, colony_size, fence_present, legacy_colony, 
-                    wgt, geometry)) %>%
-    arrange(colony_size, fence_present)
+  oversample_centroids <- sample_set$sites_over
+  experiment_centroids <- bind_rows(legacy_centroids, random_centroids, oversample_centroids) %>%
+    dplyr::select(c(colony_id, acres, colony_size, fence_present, fence_present_fake, legacy_colony, 
+                    wgt, siteuse, geometry)) %>%
+    arrange(colony_size, fence_present_fake)
   View(experiment_centroids)
   ####  NOTE: Double check each size class has 2 observations per fence classification!
   
   #'  Set new seed for reproducibility
-  set.seed(72027)
+  set.seed(45761) #72027
   
   #'  Randomly select which colonies will be treatment sites, including all legacy sites 
   #'  (non-selected colonies will default as control sites)
@@ -375,34 +455,34 @@
     sframe = experiment_centroids,                 # finite set of spatial points to choose from
     n_base = c(Small = 2, Medium = 2, Large = 2),  # sample size for each strata
     stratum_var = "colony_size",                   # column name of strata
-    caty_var = "fence_present",                    # column name of second strata with unequal sampling probability (even though in this case it is equal)
-    caty_n = c(Yes = 1, No = 1),                   # vector of sample sizes per second strata (equal sampling probability in this case)
+    caty_var = "fence_present_fake",               # column name of second strata with unequal sampling probability (even though in this case it is equal)
+    caty_n = c(Y = 1, N = 1),                      # vector of sample sizes per second strata (equal sampling probability in this case)
     legacy_var = "legacy_colony",                  # column name of legacy indicator (non-legacy colonies are NA)
-    DesignID = "Colony_id"                         # naming structure for each site's identifier selected in the sample
+    n_over = 1,
+    DesignID = "colony_id"                         # naming structure for each site's identifier selected in the sample
   )
   treatment_legacy <- treatment_set$sites_legacy
   treatment_random <- treatment_set$sites_base
-  treatment_centroids <- bind_rows(treatment_legacy, treatment_random) %>%
-    dplyr::select(c(colony_id, acres, colony_size, fence_present, legacy_colony, 
-                    wgt, geometry)) %>%
+  treatment_oversample <- treatment_set$sites_over
+  treatment_centroids <- bind_rows(treatment_legacy, treatment_random, treatment_oversample) %>%
+    dplyr::select(c(colony_id, acres, colony_size, fence_present, fence_present_fake, legacy_colony, 
+                    wgt, siteuse, geometry)) %>%
     arrange(colony_size, fence_present)
   View(treatment_centroids)
-  ####  NOTE: Double check each size class has 1 observation per fence classification!
+  ####  NOTE: Double check each size class has at least 1 observation per fence classification!
   
   #'  Save polygons of randomly selected colonies
   experiment_colonies <- colony_dat[colony_dat$colony_id %in% experiment_centroids$colony_id,] %>%
     #' Indicate which colonies are treatment vs control 
     mutate(treatment = ifelse(colony_id %in% treatment_centroids$colony_id, TRUE, FALSE)) %>%
     #'  Arrange by colony size and fence to more easily double check selection
-    arrange(colony_size, fence_present, treatment) %>%
+    arrange(colony_size, fence_present_fake, treatment) %>%
     relocate(treatment, .before = geometry)
   View(experiment_colonies)
   
   #'  Visualize 
-  #'  Distribution of colony sizes 
-  mapview(experiment_colonies, zcol = "colony_id")
   #'  Distribution of treatment and control colonies (make sure now obvious spatial pattern)
-  mapview(experiment_colonies, zcol = "treatment")
+  mapview(pd_2026_final_union) + mapview(experiment_colonies, zcol = "treatment")
   
   
   
